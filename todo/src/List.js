eef268ea01a914af78efd423121ceb34e5a98359
Igor Klein <thelastklein@gmail.com>
2021-09-23 15:56:35 -0300

    Create Delete Button

--- todo/src/List.js
+++ todo/src/List.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 function List(props){
+
+    
     
     return(<ul>
-         {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+         {props.items.map(item => <li key={item.id}>{item.text} 
+            <button onClick={()=>{props.onItemDeleted(item)}} > <img className="delimg" alt="delete" src="./assets/bin.png"></img></button>
+         </li>)}
     </ul>)
 }
 
--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -13,18 +13,23 @@ function Todo(){
     function onAddItem(text){
 
         let item = new Item(text);
-                
+
         setItems([...items, item])
     }
 
-    
+    function onItemDeleted(item){
+
+        let filteredItems = items.filter(it=>it.id != item.id)
+
+        setItems(filteredItems)
+    }
 
 
     return (<div className="container">
                 <h1>Todo</h1>
                 <TodoForm onAddItem={onAddItem}></TodoForm>
 
-                <List items={items}></List>
+                <List onItemDeleted={onItemDeleted} items={items}></List>
            </div>)
 
 }
