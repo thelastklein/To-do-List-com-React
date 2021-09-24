6cdc978ceab054c1b804a49f8664d21a84b1a526
Igor Klein <thelastklein@gmail.com>
2021-09-23 21:13:58 -0300

    Add done/undone buttons

--- todo/src/List.js
+++ todo/src/List.js
@@ -1,11 +1,23 @@
 import React from "react"
 
+function DoneImg(props){
+
+    if(props.done){
+        return (<img alt="done" src="./assets/on.png"></img>)
+    }else{
+        return (<img alt="undone" src="./assets/off.png"></img>)
+    }
+}
+
+
+
 function List(props){
 
     
     
     return(<ul>
-         {props.items.map(item => <li key={item.id}>{item.text} 
+         {props.items.map(item => <li className={item.done ? "done":""} key={item.id}>{item.text} 
+            <button onClick={()=>{props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
             <button onClick={()=>{props.onItemDeleted(item)}} > <img className="delimg" alt="delete" src="./assets/bin.png"></img></button>
          </li>)}
     </ul>)
